0e539e0f57e8066cfe09dc8ec52db4764c864a77
PhilipNaveen <64291543+PhilipNaveen@users.noreply.github.com>
2021-04-10 14:21:35 -0400

    Create Activators.py

---
+++ Activators.py
@@ -0,0 +1,26 @@
+"""
+All current activator functions are here, but more can be added.
+"""
+
+#standard class: no inherticance
+class ActivationFunctions:
+  
+  #regularized ReLU activator
+  @staticmethod
+  def relu(x):
+    return nm.maximum(0, x)
+  
+  #derivative for ReLU
+  @staticmethod
+  def relu_gradient(x):
+    return 1 if (x > 0) else 0
+
+  #regularized sigmoid activator
+  @staticmethod
+  def sigmoid(x):
+    return 1/(1+nm.exp(-x))
+
+  #derivative for sigmoid
+  @staticmethod
+  def sigmoid_gradient(x):
+    return ActivationFunctions.sigmoid(x) * (1 - ActivationFunctions.sigmoid(x))
